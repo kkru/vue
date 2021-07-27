fc52be85880b948fa8de458b6dfe52050dc10363
minseok.shin <alstjr7388@gmail.com>
2021-07-28 00:16:17 +0900

    spring boot 세팅 완료 - java run 실행 확인

---
+++ springboot/message/src/main/java/app/messages/App.java
@@ -0,0 +1,14 @@
+package app.messages;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        MessageService messageService = context.getBean(MessageService.class);
+        messageService.save("Hello Spring");
+    }
+}
---
+++ springboot/message/src/main/java/app/messages/AppConfig.java
@@ -0,0 +1,28 @@
+package app.messages;
+
+//import net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy;
+//import org.springframework.beans.factory.annotation.*;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+
+//import java.beans.BeanProperty;
+//import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("app.messages")
+public class AppConfig {
+
+    @Bean
+    public MessageRepository messageRepository() {
+        return new MessageRepository();
+    }
+
+    @Bean
+    MessageService messageService() {
+        return new MessageService(messageRepository());
+    }
+    
+}
