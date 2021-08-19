312bc7d25d1d75b0c546dbe77799bb734de1b907
minseok.shin <alstjr7388@gmail.com>
2021-08-19 21:42:08 +0900

    bean auto scan

--- springboot/message/src/main/java/app/messages/AppConfig.java
+++ springboot/message/src/main/java/app/messages/AppConfig.java
@@ -14,15 +14,5 @@ import org.springframework.context.annotation.ComponentScan;
 @Configuration
 @ComponentScan("app.messages")
 public class AppConfig {
-
-    @Bean
-    public MessageRepository messageRepository() {
-        return new MessageRepository();
-    }
-
-    @Bean
-    MessageService messageService() {
-        return new MessageService(messageRepository());
-    }
     
 }
