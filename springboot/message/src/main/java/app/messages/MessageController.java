7133ac529023171ffcc83b5d15bf1c62f8b69d14
minseok.shin <alstjr7388@gmail.com>
2021-08-19 22:15:39 +0900

    welcome.html 출력

--- springboot/message/src/main/java/app/messages/MessageController.java
+++ springboot/message/src/main/java/app/messages/MessageController.java
@@ -1,18 +1,18 @@
 package app.messages;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/message")
 public class MessageController {
     
     @GetMapping("/welcome")
-    @ResponseBody
-    public String welcome() {
-        return "Hello, Welcome to Spring Boot!";
+    public String welcome(Model model) {
+        model.addAttribute("message","Hello, Welcome to Spring Boot!!");
+        return "welcome";
     }
     
 }
