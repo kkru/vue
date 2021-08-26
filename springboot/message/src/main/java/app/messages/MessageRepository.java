8ef96d08ec5cfc7c3f40f7a100903879c53c73a6
minseok.shin <alstjr7388@gmail.com>
2021-08-26 22:48:21 +0900

    MYSQL 연동, INSERT DATA 완료, properties 파일 설정 주의사항 application.properties 파일 중 datasource.url 참고

--- springboot/message/src/main/java/app/messages/MessageRepository.java
+++ springboot/message/src/main/java/app/messages/MessageRepository.java
@@ -1,17 +1,66 @@
 package app.messages;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+
+import javax.sql.DataSource;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageRepository {
-    
-    private final static Log log = LogFactory.getLog(MessageRepository.class);
 
-    public void saveMessage(Message message){
+    private final static Log log = LogFactory.getLog(MessageRepository.class);
+    private DataSource dataSource;
+
+    public MessageRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public Message saveMessage(Message message){
         // 데이터베이스에 메시지를 저장한다.
         log.info("Saved message: " + message.getText());
+
+        Connection c = DataSourceUtils.getConnection(dataSource);
+
+        try{
+            String insertSql = "INSERT INTO messages (`id`, `text`, `created_date`) VALUE (null, ? , ?)";
+
+            PreparedStatement ps = c.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, message.getText());
+            ps.setTimestamp(2, new Timestamp(message.getCreatedDate().getTime()));
+            int rowsAffected = ps.executeUpdate();
+
+            if(rowsAffected > 0){
+                ResultSet result = ps.getGeneratedKeys();
+                if(result.next()){
+                    int id = result.getInt(1);
+                    return new Message(id, message.getText(), message.getCreatedDate());
+                } else {
+                    log.error("Failed to retrieve id, No row in result set");
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        } catch (SQLException ex) {
+            log.error("Failed to save message", ex);
+            try {
+                c.close();
+            } catch (SQLException e) {
+                log.error("Failed to close connection" , e);
+            }
+        } finally {
+            DataSourceUtils.releaseConnection(c, dataSource);
+        }
+        return null;
     }
 
 }
